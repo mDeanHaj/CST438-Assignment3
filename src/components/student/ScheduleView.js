6620dcdc5a8dd4162f3fca10bfdc3a4af6ceec6c
harisj739 <hjilani@csumb.edu>
2024-07-09 16:11:35 -0700

    Fixed issues with "ScheduleView.js" by adding input parameters that are used to fetch the student schedule for that term.
    ISSUE: The sectionId doesn't show up, needs to be looked at and fixed.

--- src/components/student/ScheduleView.js
+++ src/components/student/ScheduleView.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { SERVER_URL } from '../../Constants';
+import {useLocation} from "react-router-dom";
 
 // student can view schedule of sections 
-// use the URL /enrollment?studentId=3&year= &semester=
+// use the URL /enrollment?studentId=3&year=&semester=
 // The REST api returns a list of EnrollmentDTO objects
 // studentId=3 will be removed in assignment 7
 
@@ -9,14 +11,111 @@ import React, {useState} from 'react';
 // issue a DELETE with URL /enrollment/{enrollmentId}
 
 const ScheduleView = (props) => {
-    
-   
-    return(
-        < > 
-            <h3>Not implemented</h3>
-        </ >
-    );
+    const [schedules, setSchedules] = useState([]);
+    const [message, setMessage] = useState('');
+    const [schedule, setSchedule] = useState({year:'', semester:''});
 
+
+    const fetchSchedule = async () => {
+        if (schedule.year==='' || schedule.semester==='') {
+            setMessage("Enter search parameters");
+        }
+        else {
+            try {
+                const response = await fetch(`${SERVER_URL}/enrollments?studentId=3&year=${schedule.year}&semester=${schedule.semester}`);
+                if (response.ok) {
+                    const json = await response.json();
+                    setSchedules(json);
+                } else {
+                    const json = await response.json();
+                    setMessage("response error: " + json.message);
+                }
+            } catch (err) {
+                setMessage("network error: " + err);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchSchedule();
+    }, []);
+
+    const editChange = (event) => {
+        setSchedule({...schedule,  [event.target.name]:event.target.value});
+    }
+
+    const dropCourse = async (enrollmentId) => {
+        try {
+            const response = await fetch(`${SERVER_URL}/enrollments/${enrollmentId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                setMessage("Course dropped");
+                fetchSchedule();
+            } else {
+                const json = await response.json();
+                setMessage("response error: " + json.message);
+            }
+        } catch (err) {
+            setMessage("network error: " + err);
+        }
+    };
+
+    return (
+        <>
+            <h3>Your Schedule</h3>
+            <h4>{message}</h4>
+            <h4>Enter term year and semester. Example 2024 Spring.</h4>
+            <table className="Center">
+                <tbody>
+                <tr>
+                    <td>Year:</td>
+                    <td><input type="text" id="year" name="year" value={schedule.year} onChange={editChange}/></td>
+                </tr>
+                <tr>
+                    <td>Semester:</td>
+                    <td><input type="text" id="semester" name="semester" value={schedule.semester}
+                               onChange={editChange}/></td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <button type="submit" id="search" onClick={fetchSchedule}>Search for Schedule</button>
+            <br/>
+            <br/>
+            <table className="Center">
+                <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Semester</th>
+                    <th>CourseId</th>
+                    <th>SectionId</th>
+                    <th>Title</th>
+                    <th>Credits</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {schedules.map((s) => (
+                    <tr key={s.enrollmentId}>
+                        <td>{s.year}</td>
+                        <td>{s.semester}</td>
+                        <td>{s.courseId}</td>
+                        <td>{s.secId}</td>
+                        <td>{s.title}</td>
+                        <td>{s.credits}</td>
+                        <td>
+                            <button onClick={() => dropCourse(s.enrollmentId)}>Drop</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
 
 export default ScheduleView;
