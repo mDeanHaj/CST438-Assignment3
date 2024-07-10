3e20a7708e00dbbd6e7d027894dafe11bc39d39c
davethewave88 <126984488+davethewave88@users.noreply.github.com>
2024-07-09 19:56:46 -0700

    Update AssignmentsStudentView.js
    updated assignmentstudentview file

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import SectionUpdate from "../admin/SectionUpdate";
+import Button from "@mui/material/Button";
+import SectionAdd from "../admin/SectionAdd";
+import {SERVER_URL} from "../../Constants";
 
 // student views a list of assignments and assignment grades 
 // use the URL  /assignments?studentId= &year= &semester=
@@ -8,12 +12,72 @@ import React, {useState} from 'react';
 // display a table with columns  Course Id, Assignment Title, Assignment DueDate, Score
 
 const AssignmentsStudentView = (props) => {
-    
-     
+
+    const [assignments, setAssignments] = useState([]);
+    const headers = ['CousreId', 'Assignment Title', 'Assignment DueDate',  'Score'];
+    const [message, setMessage] = useState('');
+    const [search, setSearch] = useState({year:'', semester:''});
+
+    const fetchAssignments = async () => {
+        if (search.year==='' || search.semester==='' ) {
+            setMessage("Enter search parameters");
+        } else {
+            try {
+                const response = await fetch(`${SERVER_URL}/assignments?studentId=3&year=${search.year}&semester=${search.semester}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setAssignments(data);
+                } else {
+                    const rc = await response.json();
+                    setMessage(rc.message);
+                }
+            } catch(err) {
+                setMessage("network error: "+err);
+            }
+        }
+    }
+
+
     return(
-        <> 
-            <h3>Not implemented</h3>   
-        </>
+        <div>
+            <h3>Assignments</h3>
+
+            <h4>{message}</h4>
+            <h4>Enter year and semester.  Example 2024 Spring</h4>
+            <table className="Center">
+                <tbody>
+                <tr>
+                    <td>Year:</td>
+                    <td><input type="text" id="syear" name="year" value={search.year} onChange={editChange} /></td>
+                </tr>
+                <tr>
+                    <td>Semester:</td>
+                    <td><input type="text" id="ssemester" name="semester" value={search.semester} onChange={editChange} /></td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <button type="submit" id="search" onClick={fetchAssignments} >Search for Assignments</button>
+            <br/>
+            <br/>
+            <table className="Center" >
+                <thead>
+                <tr>
+                    {headers.map((a, idx) => (<th key={idx}>{a}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                {assignments.map((a) => (
+                    <tr key={a.title}>
+                        <td>{a.courseId}</td>
+                        <td>{a.title}</td>
+                        <td>{a.dueDate}</td>
+                        <td>{a.score}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
